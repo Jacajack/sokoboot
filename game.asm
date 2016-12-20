64cdf9b33831e443b09aa7fe5ec4bc1c9882e804
Jacek Wieczorek <mrjjot@gmail.com>
2016-12-21 00:40:04 +0100

    Add basic movement and game logics (buggy tho)(WIP commit)

--- game.asm
+++ game.asm
@@ -20,9 +20,55 @@ int 0x10
 ;Game logics
 ;Render
 
-mov cl, 2
+mov cl, 1
 mov ch, 1
 call putplayer
+mov cl, 1
+mov ch, -1
+call movplayer
+
+myloop:
+mov al, 0						;Get character
+mov ah, 0x00					;
+int 0x16
+
+;call puthex
+
+;jmp myloop
+
+;mov cl, 1
+;mov ch, 0
+;call movplayer
+;jmp myloop
+
+push ax
+cmp al, 'a'
+mov cl, -1
+mov ch, 0
+je ok
+
+cmp al, 'd'
+mov cl, 1
+mov ch, 0
+je ok
+
+cmp al, 'w'
+mov cl, 0
+mov ch, -1
+je ok
+
+cmp al, 's'
+mov cl, 0
+mov ch, 1
+je ok
+
+mov cx, 0
+
+
+ok:
+pop ax
+
+call movplayer
 
 mov cl, 0
 mov ch, 32
@@ -30,6 +76,7 @@ mov dl, 0
 mov dh, 20
 call drawmap
 
+jmp myloop
 jmp $
 
 ;Fetch keystroke
@@ -96,7 +143,7 @@ drawsprite:
 	drawsprite_x: dw 0
 	drawsprite_y: dw 0
 
-;Draws part of map on screen
+;Draws part of map on screenmyloop:
 ;cl - start x position
 ;ch - width
 ;dl - start y position
@@ -153,14 +200,155 @@ drawmap:
 putplayer:
 	pushf
 	pusha
-	mov [player_x], cl				;Store player position
-	mov [player_y], ch				;
+	mov [player_pos], cx			;Store player position
 	call getmapaddr					;Get address of field where player should be put
 	mov byte [bx], 5				;Put 5 (player) on map
 	popa
 	popf
 	ret
 
+;Moves player around the map
+;cl - x movement
+;ch - y movement
+movplayer:
+	pushf
+	pusha
+
+	mov [movplayer_delta], cx
+	mov dx, [player_pos]
+	add dl, cl
+	add dh, ch
+	mov [movplayer_dest], dx
+
+	;Bound check
+	cmp dl, 32
+	jge movplayer_end
+	cmp dh, 20
+	jge movplayer_end
+
+	;Get dest field content
+	push cx
+	mov cx, dx
+	call getmapaddr
+	pop cx
+
+
+	;wall
+	cmp byte [bx], 1
+	je movplayer_end
+
+	;box
+	cmp byte [bx], 2
+	je movplayer_box
+	cmp byte [bx], tile_socketbox
+	je movplayer_box
+	jmp movplayer_move
+	movplayer_box:
+		pusha									;Store all registers
+		mov cx, [movplayer_delta]				;Get player movement direction
+		mov dx, [movplayer_box_position]		;Get box position
+		mov ax, dx								;Add another delta to box position to get box destination
+		add al, cl								;
+		add ah, ch								;
+		mov [movplayer_box_dest], ax			;Store box destination
+
+		cmp al, 32
+		jge movplayer_box_abort
+		cmp ah, 20
+		jge movplayer_box_abort
+
+
+
+
+
+		mov cx, [movplayer_box_dest]			;Get box destination
+		call getmapaddr
+		mov dl, byte [bx]
+		mov [movplayer_box_tile], dl
+
+		cmp byte [bx], tile_socket				;Check if destination is socket
+		je movplayer_box_socket					;Jump to socket code
+		cmp byte [bx], tile_air
+		je movplayer_box_air
+		jmp movplayer_box_abort
+
+		movplayer_box_socket:					;Destination tile is socket
+		mov byte [bx], tile_socketbox			;Place a socketbox
+		jmp movplayer_box_grab					;Proceed to destroy old box
+
+		movplayer_box_air:						;Destination tile is air
+		mov byte [bx], tile_box					;Place a box
+		jmp movplayer_box_grab
+
+
+		movplayer_box_grab:
+		mov cx, [movplayer_dest]		;Get current box position
+		call getmapaddr							;Get current box address to bx
+
+		mov dl, [movplayer_box_tile]
+		;cmp dl, tile_socket
+		;je movplayer_box_grab_socketbox
+		jmp movplayer_box_grab_box
+
+		movplayer_box_grab_socketbox:
+		mov byte [bx], tile_socket
+		jmp movplayer_box_ok
+
+		movplayer_box_grab_box:
+		mov byte [bx], tile_socket
+		jmp movplayer_box_ok
+
+
+		movplayer_box_abort:
+		popa
+		jmp movplayer_end
+
+		movplayer_box_ok:
+		popa
+		jmp movplayer_move
+
+
+
+	;Check current tile
+	movplayer_move:
+	mov cx, [player_pos]
+	call getmapaddr
+	cmp byte [bx], 6
+	je movplayer_move_socket
+	cmp byte [bx], 2
+	jmp movplayer_move_air
+
+	movplayer_move_air:
+	mov byte [bx], 0
+	jmp movplayer_move_place
+	movplayer_move_socket:
+	mov byte [bx], 3
+	jmp movplayer_move_place
+
+	movplayer_move_place:
+	mov [player_pos], dx
+	mov cx, dx
+	call getmapaddr
+	cmp byte [bx], 3
+	je movplayer_move_place_socket
+	jmp movplayer_move_place_air
+
+	movplayer_move_place_air:
+	mov byte [bx], 5
+	jmp movplayer_end
+	movplayer_move_place_socket:
+	mov byte [bx], 6
+	jmp movplayer_end
+
+	movplayer_end:
+	popa
+	popf
+	ret
+	movplayer_delta: dw 0
+	movplayer_box_position:
+	movplayer_dest: dw 0
+	movplayer_box_dest: dw 0
+	movplayer_box_tile: db 0
 
 ;Return requested map field address
 ;cl - x position
@@ -182,8 +370,7 @@ getmapaddr:
 	ret
 	getmapaddr_addr: dw 0
 
-player_x: db 0 	;Player x position
-player_y: db 0 	;Player y position
+player_pos: dw 0 	;Player x position
 
 map:			;Map data
 	db 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
@@ -195,7 +382,7 @@ map:			;Map data
 	db 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1
 	db 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1
 	db 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1
-	db 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1
+	db 1, 0, 0, 0, 3, 1, 0, 0, 0, 2, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1
 	db 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1
 	db 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1
 	db 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1
