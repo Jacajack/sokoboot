aa4c82de34941161f768802a7b4cfa2f2b05353f
Jacek Wieczorek <mrjjot@gmail.com>
2017-02-26 00:42:18 +0100

    Improve img2bin script just a little bit

--- splash/img2bin.py
+++ splash/img2bin.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import sys
+import math
 import Image
 
 image = Image.open( "splash.png" );
@@ -8,8 +9,7 @@ image = image.convert( "RGB" );
 for y in range( 0, 200 ):
 	for x in range( 0, 320 ):
 		r, g, b = image.getpixel( ( x, y ) );
-		bright = ( r + g + b ) / 255;
-		if ( bright == 0 ):
-			sys.stdout.write( b"\x10" );
-		else:
-			sys.stdout.write( b"\x31" );
+		r = int( math.floor( r / 128 ) );
+		g = int( math.floor( g / 128 ) );
+		b = int( math.floor( b / 128 ) );
+		sys.stdout.write( unichr( b | ( g << 1 ) | ( r << 2 ) ) );
