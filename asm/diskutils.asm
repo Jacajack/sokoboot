f728aa0fa117d6ed0a333f4a3aa246df7e2fc77c
Jacek Wieczorek <mrjjot@gmail.com>
2017-06-28 12:17:53 +0200

    Add LBA based disk reading routine

--- asm/diskutils.asm
+++ asm/diskutils.asm
@@ -9,6 +9,41 @@ diskreset:
 	popf
 	ret
 
+;ax - LBA
+;dl - drive number
+;dh - sector count
+;es:bx - starting addresses
+diskloadlba:
+	pushf
+	pusha
+	push dx							;Store dx value
+	mov dx, 0						;Clear reminder register
+	mov cx, n_s						;Divide LBA by sector per track count
+	div cx							;
+	inc dx							;Increment reminder
+	mov [diskloadlba_s], dx			;Save reminder as sector number
+	mov dx, 0						;Clear reminder register
+	mov cx, n_h						;Divide LBA by heads per cylinder count
+	div cx							;
+	mov [diskloadlba_h], dx			;Reminder is head number
+	mov [diskloadlba_c], ax			;Quotient is cylinder number
+	pop dx							;Restore dx
+	mov al, dh						;Load sectors amount into al
+	mov cl, [diskloadlba_s]			;Load CHS
+	mov ch, [diskloadlba_c]			;
+	mov dh, [diskloadlba_h]			;
+	mov ah, 0x2 					;Sector read operation
+	int 0x13						;Disk interrupt
+	jc diskload_error				;Handle disk error
+	popa
+	popf
+	ret
+	diskloadlba_s: dw 0
+	diskloadlba_h: dw 0
+	diskloadlba_c: dw 0
+	n_s equ 18
+	n_h equ 2
+
 ;Loads sectors into RAM
 ;al - sectors amount
 ;bx - starting address
