f23a81594b8120d9d883859f530d8855bc4606df
Jacek Wieczorek <mrjjot@gmail.com>
2017-07-03 02:03:04 +0200

    Add working splash loader (I really hope so :confused:)

--- asm/init.asm
+++ asm/init.asm
@@ -33,25 +33,41 @@ jmp $
 splashload:
 	pushf
 	pusha
-	push es									;Store extra segment register
-	mov dl, [boot_drive]					;Set drive number
-	mov dh, 1								;Read 1 sector each time
-	mov bx, 0xA000							;Load es through bx
-	mov es, bx								;Setup segment register to point video memory
-	mov bx, 0								;Reset offset register
-	mov ax, splash_sector					;We start reading at sector 18
-	splash_l:								;Loop
-		call diskrlba						;Load data from disk
-		add bx, 512							;Increment memory pointer
-		inc ax								;Increment sector pointer
-		cmp ax, splash_sector+splash_len	;We want to read 125 sectors
-		jbe splash_l						;Loop
-	pop es									;Restore segment register
+	push ds
+	push es
+	mov ax, splash_sector			;Sector counter
+	mov cx, 0						;Address counter
+	splash_l:
+		mov dl, [boot_drive]		;Set drive number
+		mov dh, 1					;Read 1 sector each time
+		mov bx, splash_memseg		;Setup buffer segment
+		mov es, bx					;
+		mov bx, splash_memaddr		;Buffer address
+		call diskrlba				;Load data from disk
+		mov dx, splash_memseg		;Setup source segment (buffer)
+		mov ds, dx					;
+		mov si, splash_memaddr		;Setup source address
+		mov dx, 0xA000				;Setup destination segment (video memory)
+		mov es, dx					;
+		mov di, cx					;Setup destination address
+		push cx						;Store address counter value
+		mov cx, 512					;Copy 512b
+		cld							;Clear direction flag (growing addresses)
+		rep movsb					;Repeat byte copy operation
+		pop cx						;Restore address counter
+		add cx, 512					;Increment address counter
+		inc ax						;Increment sector counter
+		cmp ax, splash_sector+splash_len	;Check loop condition
+		jbe splash_l				;Loop
+	pop es
+	pop ds
 	popa
 	popf
 	ret
 	splash_sector equ 18
 	splash_len equ 125
+	splash_memaddr equ 0xf000
+	splash_memseg equ 0x0000
 
 boot_drive: db 0
 
