199accbce46e8311e73df401b8cd2431f75d4329
Jacek Wieczorek <mrjjot@gmail.com>
2017-07-07 12:12:44 +0200

    Make diskerr routine rely on stdio library

--- asm/disk.asm
+++ asm/disk.asm
@@ -153,23 +153,27 @@ diskreset:
 	ret
 
 ;Disk error handler
+;dl - disk number
 diskerr:
 	pushf
 	pusha
+	call gotext
 	mov si, diskerr_mesg	;Load message address into si
-	mov ah, 0xE				;Setup ah for interrupt
-	diskerr_l1:				;Puts loop
-		mov al, [si]		;Load into al
-		cmp al, 0			;Compare character with 0
-		je diskerr_end		;If equal, end run
-		int 0x10			;Run interrupt
-		inc si				;Increment counter
-		jmp diskerr_l1		;Loop
-	diskerr_end:
-	jmp $
+	call puts				;Display the message
+	mov ah, 1				;Get status of last operation
+	int 0x13				;Run interrupt
+	mov al, ah				;Display error code
+	call puthexb			;
+	mov si, diskerr_mesg_nl	;Print new line
+	call puts				;
+	jmp $					;Endless loop
 	popa
 	popf
 	ret
-	diskerr_mesg:
-		db "[critical] disk fault!", 10, 13
-		db 0
+	diskerr_mesg: db "disk fault - errno: ", 0
+	diskerr_mesg_nl: db 10, 13, 0
+	diskerr_hex: db "0123456789abcdef"
+
+%include "stdio/puthex.asm"
+%include "stdio/puts.asm"
+%include "stdio/gotext.asm"
