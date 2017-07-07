81dc1ad8172f5625d5733d1853d5136eed03f1be
Jacek Wieczorek <mrjjot@gmail.com>
2017-07-07 15:46:01 +0200

    Make splash use 256 color palette

--- splash/img2bin.py
+++ splash/img2bin.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 import sys
 import math
+import os
 import Image
 
 image = Image.open( "splash.png" );
 image = image.convert( "RGB" );
+binstdout = os.fdopen( sys.stdout.fileno( ), "wb" )
 
 for y in range( 0, 200 ):
 	for x in range( 0, 320 ):
 		r, g, b = image.getpixel( ( x, y ) );
-		r = int( math.floor( r / 128 ) );
-		g = int( math.floor( g / 128 ) );
-		b = int( math.floor( b / 128 ) );
-		sys.stdout.write( unichr( b | ( g << 1 ) | ( r << 2 ) ) );
+		r = int( math.floor( r / 32 ) );
+		g = int( math.floor( g / 32 ) );
+		b = int( math.floor( b / 64 ) );
+		binstdout.write( chr( ( b << 6 ) | ( g << 3 ) | r ) );
