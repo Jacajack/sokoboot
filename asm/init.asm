3c1dfca0f50201a0dfe14003f843b8b1bc23bd1e
Jacek Wieczorek <mrjjot@gmail.com>
2017-07-12 01:54:42 +0200

    Optimize splash loader (few times faster on my COMPAQ)

--- asm/init.asm
+++ asm/init.asm
@@ -36,39 +36,52 @@ splashload:
 	pusha
 	push ds
 	push es
-	mov ax, splash_sector			;Sector counter
-	mov cx, 0						;Address counter
-	splash_l:
-		mov dl, [boot_drive]		;Set drive number
-		mov dh, 1					;Read 1 sector each time
-		mov bx, splash_memseg		;Setup buffer segment
-		mov es, bx					;
-		mov bx, splash_memaddr		;Buffer address
-		call diskrlba				;Load data from disk
-		mov dx, splash_memseg		;Setup source segment (buffer)
-		mov ds, dx					;
-		mov si, splash_memaddr		;Setup source address
-		mov dx, 0xA000				;Setup destination segment (video memory)
-		mov es, dx					;
-		mov di, cx					;Setup destination address
-		push cx						;Store address counter value
-		mov cx, 512					;Copy 512b
-		cld							;Clear direction flag (growing addresses)
-		rep movsb					;Repeat byte copy operation
-		pop cx						;Restore address counter
-		add cx, 512					;Increment address counter
-		inc ax						;Increment sector counter
+	mov ax, splash_sector					;Sector counter
+	mov cx, 0								;Address counter
+	splashload_l:							;
+		mov dl, [boot_drive]				;Set drive number
+		mov dh, splash_sector_portion		;Read 1 sector each time
+		mov bx, splash_memseg				;Setup buffer segment
+		mov es, bx							;
+		mov bx, splash_memaddr				;Buffer address
+		call diskrlba						;Load data from disk
+		mov dx, splash_memseg				;Setup source segment (buffer)
+		mov ds, dx							;
+		mov si, splash_memaddr				;Setup source address
+		mov dx, 0xA000						;Setup destination segment (video memory)
+		mov es, dx							;
+		push cx								;Store address counter value
+		mov di, cx							;Setup destination address
+		mov cx, splash_len					;Check how many sectors are left
+		sub cx, ax							;If less than one portion, then copy data partially
+		cmp cx, splash_sector_portion - 1	;
+		splashload_full:					;Full copy
+		mov cx, 512	* splash_sector_portion	;Copy 512b * data portion size
+		jmp splashload_memcpy				;
+		splashload_rest:					;Partial copy
+		shl cx, 9							;Multilply number of left sectors with 512
+		splashload_memcpy:					;Copy data
+		cld									;Clear direction flag (growing addresses)
+		rep movsb							;Repeat byte copy operation
+		pop cx								;Restore address counter
+		add cx, 512 * splash_sector_portion	;Increment address counter
+		add ax, splash_sector_portion		;Increment sector counter
 		cmp ax, splash_sector+splash_len	;Check loop condition
-		jbe splash_l				;Loop
+		jbe splashload_l						;Loop
 	pop es
 	pop ds
 	popa
 	popf
 	ret
+	splash_sector_portion equ 18
 	splash_sector equ 18
 	splash_len equ 125
-	splash_memaddr equ 0xf000
+	splash_memaddr equ 0x4000
 	splash_memseg equ 0x0000
+
+%if splash_sector % 18 != 0
+%error "Splash data has to be located at the beginig of the track!"
+%endif
 
 boot_drive: db 0
 
