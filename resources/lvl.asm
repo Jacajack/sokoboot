e5849e1d2fca30834baaf71909e7577ea2f6b6d1
Jacek Wieczorek <mrjjot@gmail.com>
2017-07-11 18:24:38 +0200

    New (old) test level

--- resources/lvl.asm
+++ resources/lvl.asm
@@ -3,14 +3,31 @@ lvldata:
 	lvldata_id: dw 0
 	lvldata_name: times 80 db 0
 	lvldata_desc: times 320 db 0
-	lvldata_playerpos: dw 0, 0
-	lvldata_size: dw 32, 20
+	lvldata_playerx: dw 0
+	lvldata_playery: dw 0
+	lvldata_width: dw 20
+	lvldata_height: dw 12
+	lvldata_camx: dw 0
+	lvldata_camy: dw 0
 	lvldata_flags: dw 0, 0
 	lvldata_next: dw 0
 	lvldata_reserved: times 1024 - ( $ - lvldata ) db 0
-	lvldata_map: 
-	
-	times 40 db 0
-	db 5, 0, 0, 1, 2, 0, 0, 3, 
-	
+	lvldata_map:
+
+	map:								;Map data
+		db 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+		db 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+		db 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0
+		db 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0
+		db 0, 0, 0, 0, 0, 0, 1, 3, 5, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0
+		db 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 2, 3, 1, 0, 0, 0, 0, 0, 0, 0
+		db 0, 0, 0, 0, 0, 0, 1, 3, 1, 1, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0
+		db 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 3, 0, 1, 1, 0, 0, 0, 0, 0, 0
+		db 0, 0, 0, 0, 0, 0, 1, 2, 0, 4, 2, 2, 3, 1, 0, 0, 0, 0, 0, 0
+		db 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 1, 0, 0, 0, 0, 0, 0
+		db 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0
+		db 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+
+
+
 	times 65536 - ( $ - lvldata ) db 0
