90e65d772cb1efa2be6bf20a6a8d0eda704119d3
Jacek Wieczorek <mrjjot@gmail.com>
2017-07-14 23:47:21 +0200

    Improve atoi routine (ignores bad chars at the end)

--- asm/stdio/atoi.asm
+++ asm/stdio/atoi.asm
@@ -3,6 +3,8 @@
 
 ;Parses string pointed by si and returns its decimal value in ax
 ;Supports only unsigned integers
+;If strings cointains some unsupported characters at the end they are ignored
+;If two digits are separated by unsupported character string is considered bad
 ;si - string
 ;return ax - value
 ;return cf - if set - bad string
@@ -12,17 +14,25 @@ atoi:
 	mov ax, 0				;Clear value register
 	mov bx, 0				;Clear character register
 	mov cx, 10				;Multiplier
+	mov dl, 0				;Register for 'bad character' flag
 	atoi_l:					;Main loop
 		mov bl, [si]		;Get character from string
 		test bl, bl			;Check if it's zero
 		jz atoi_end			;If it's zero, quit
 		sub bl, '0'			;Else, substract 48 from it
-		jc atoi_bad			;On overroll, assume string to be bad
+		jc atoi_bad			;On overroll, assume char to be bad
 		cmp bl, 9			;If the result is greater than 9
-		ja atoi_bad			;Also assume string to be bad
+		ja atoi_bad			;Also assume char to be bad
 		mul cx				;Multiply value register
-		jo atoi_bad			;On overflow, assume string to be invalid
+		jo atoi_abort		;On overflow, assume string to be invalid
 		add ax, bx			;Add new character value to the buffer
+		atoi_ok:			;
+		test dl, 1			;Check if we've encountered any bad characters
+		jnz atoi_abort		;If so, abort
+		inc si				;Else, go on
+		jmp atoi_l			;Loop
+		atoi_bad:			;
+		mov dl, 1			;Set 'first bad character flag'
 		inc si				;Increment pointer
 		jmp atoi_l			;Loop
 	atoi_end:				;
@@ -32,7 +42,7 @@ atoi:
 	popf					;Restore flags
 	clc						;Clear carry flag - success
 	ret						;
-	atoi_bad:				;
+	atoi_abort:				;
 	popa					;Restore registers
 	mov ax, 0				;Write 0 to ax
 	popf					;Restore flags
