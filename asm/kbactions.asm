b4ba4be1e6a7e257056ed1b6d2fadf347d2529e9
Jacek Wieczorek <mrjjot@gmail.com>
2017-07-18 19:41:37 +0200

    Rename keyactions to kbactions + new quit mechanism

--- asm/kbactions.asm
+++ asm/kbactions.asm
@@ -1,52 +1,56 @@
 ;Move player right and redraw drawstack contents
 kbaction_player_mover:
-	pusha
 	pushf
+	pusha
 	mov dl, 2
 	mov dh, 1
 	call movplayer
 	call drawstack_draw
-	popf
 	popa
+	popf
 	ret
 
 ;Move player up and redraw drawstack contents
 kbaction_player_moveu:
-	pusha
 	pushf
+	pusha
 	mov dl, 1
 	mov dh, 0
 	call movplayer
 	call drawstack_draw
-	popf
 	popa
+	popf
 	ret
 
 ;Move player down and redraw drawstack contents
 kbaction_player_moved:
-	pusha
 	pushf
+	pusha
 	mov dl, 1
 	mov dh, 2
 	call movplayer
 	call drawstack_draw
-	popf
 	popa
+	popf
 	ret
 
 ;Move player left and redraw drawstack contents
 kbaction_player_movel:
-	pusha
 	pushf
+	pusha
 	mov dl, 0
 	mov dh, 1
 	call movplayer
 	call drawstack_draw
-	popf
 	popa
+	popf
 	ret
 
-kbaction_reload_level:
-	pop bx
-	mov bx, 0
-	jmp lvlprompt
+;Abandon game
+kbaction_quit:
+	pushf
+	pusha
+	mov byte [game_quitrq], 1
+	popa
+	popf
+	ret
